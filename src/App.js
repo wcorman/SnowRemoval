84ab591054c1f972ed348b209c984095e2d96ba4
Wes Corman <wcorman@gmail.com>
2019-11-04 00:28:47 -0600

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import NavBar from './components/navBar';
 import Pricing from './components/pricing';
 
 function App() {
   return (
     <div className="App">
-    <Pricing />
+      <NavBar />
+      <Pricing />
     </div>
   );
 }
---
+++ src/components/navBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function NavBar() {
+  return (
+  <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home">Snow Removal</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#services">Services</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+
+  </Navbar>
+  );
+}
+
+export default NavBar;
